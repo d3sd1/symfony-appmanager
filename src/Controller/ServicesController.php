dca2ec19221efe99ce21678fc90a678bd3c3b275
Andrei García (Sobremesa) <andreigarciacuadra@gmail.com>
2019-05-17 08:07:55 +0200

    fix for offset

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -20,6 +20,9 @@ class ServicesController extends AbstractController
         $resultsPerPage = isset($_GET['r']) ? $_GET['r'] : 0;
         $totalResults = $this->getDoctrine()->getManager()->createQueryBuilder()->select('COUNT(s)')->from('App:Servicio', 's')->getQuery()->getSingleScalarResult();
 
+        if ($startIndex >= $totalResults) {
+            $startIndex = $totalResults - $resultsPerPage;
+        }
         if ($startIndex < 0) {
             $startIndex = 0;
         }
@@ -28,9 +31,6 @@ class ServicesController extends AbstractController
         }
         $currentPag = (int)($startIndex / $resultsPerPage);
 
-        if ($startIndex >= $totalResults) {
-            $startIndex = $totalResults - $resultsPerPage;
-        }
 
         $endIndex = $startIndex + $resultsPerPage;
         if ($endIndex - $startIndex < $resultsPerPage) {
@@ -42,10 +42,10 @@ class ServicesController extends AbstractController
 
         /*
          * campo: id_servicio_estado --> tabla servicio_estado
-         *   el único valor que no nos valdría es el 5 = anulado pero dependiendo de lo que queramos mostrar si son solo los abiertos sería to do lo que sea < 3 (cerrado, facturado, anulado quedarían fuera)
-         *   si necesitamos tener las fechas de referencia de cuando se realiza el servicio esto se sacaría haciendo el join servicio_categoria on servicio_categoria.id_servicio = servicio.id  donde el id_persona_servicio_estado sea menor que 9 que es el anulado y en base al campo "fecha".
+         *   el ï¿½nico valor que no nos valdrï¿½a es el 5 = anulado pero dependiendo de lo que queramos mostrar si son solo los abiertos serï¿½a to do lo que sea < 3 (cerrado, facturado, anulado quedarï¿½an fuera)
+         *   si necesitamos tener las fechas de referencia de cuando se realiza el servicio esto se sacarï¿½a haciendo el join servicio_categoria on servicio_categoria.id_servicio = servicio.id  donde el id_persona_servicio_estado sea menor que 9 que es el anulado y en base al campo "fecha".
          *
-            hay otras tablas relacionadas sin foreign key declarada como "id_presupuesto" o "id_cliente" que no sé serían necesarias.
+            hay otras tablas relacionadas sin foreign key declarada como "id_presupuesto" o "id_cliente" que no sï¿½ serï¿½an necesarias.
          */
         $qb->setFirstResult($startIndex);
         $qb->setMaxResults($resultsPerPage);
